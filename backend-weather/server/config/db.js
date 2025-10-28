7d331fa3dfe219351028cfc9cc72e4b7b4f1def4
KarandeepKaur266 <69587532+KarandeepKaur266@users.noreply.github.com>
2025-10-28 12:57:27 +0530

    Change MongoDB connection string to local instance

--- backend-weather/server/config/db.js
+++ backend-weather/server/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://karandeepkaur266:Karan99@cluster0.uvek08m.mongodb.net/weather").then(() => {
+mongoose.mongoose.connect("mongodb://127.0.0.1:27017/weather").then(() => {
     console.log("DB Connected");
 }).catch((err) => {
     console.log("Error in db connection", err);
 })
+
 
 
